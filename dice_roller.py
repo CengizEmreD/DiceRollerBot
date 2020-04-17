de657313bb6b3dee2161257e1b42bec27ed9419b
Emre Dedeağaç <emre.dedeagac@ozu.edu.tr>
2020-04-17 20:30:12 +0300

    Added help and dnd dice commands.

--- dice_roller.py
+++ dice_roller.py
@@ -4,6 +4,16 @@ import discord
 import random
 import json
 
+def roll(die):
+    result = [random.randint(1,int(die[1])) for i in range(int(die[0]))]
+    return result + [sum(result)]
+
+def unroll_array(arr):
+    st = ''
+    for value in arr:
+        st += f' [{value}] '
+    return st
+
 def DiceRoller():
     # pylint: disable=unused-variable
 
@@ -15,21 +25,43 @@ def DiceRoller():
     @client.event
     async def on_message(message):
 
-        # id = message.author.id   # TODO Will use to include username in the message
-        
+        # Get user name
+        id = message.author.id   
+        name = message.server.get_member(id).nick
+
+        # Get message content
+        content = message.content
+
         if message.author == client.user:
             return
 
         if message.content.startswith('!rollf'):
             try:
-                dice = ['-', '-', ' ', ' ', '+', '+']
-                msg = f'Rolled [{random.choice(dice)}][{random.choice(dice)}][{random.choice(dice)}][{random.choice(dice)}]'
+                die = ['-', '-', ' ', ' ', '+', '+']
+                result = [random.choice(die) for i in range(4)]
+                res = 0
+                for i in result:
+                    if i == '+':
+                        res += 1
+                    elif i == '-':
+                        res -= 1
+                msg = f'{name} rolled {res} ({unroll_array(result)}).'
                 await client.send_message(message.channel, msg)
             except:
+                await client.send_message(message.channel, 'Invalid roll.')
                 return
-        elif message.content.startswith('!rolld'):
-            pass
-
+        elif message.content.startswith('!roll help'):
+            msg = 'Commands:\n+ !rollf: Rolls 4df\n+ !roll <n>d<k>: Rolls k sided die n times. ex: !roll 2d5\n'
+            await client.send_message(message.channel, msg)
+        elif message.content.startswith('!roll'):
+            try:
+                die = content.split(' ')[1].split('d') # Getting die information
+                result = roll(die)
+                msg = f'{name} rolled {result[-1]} ({unroll_array(result[:-1])}).'
+                await client.send_message(message.channel, msg)
+            except:
+                await client.send_message(message.channel, 'Invalid roll.')
+                return
 
     @client.event
     async def on_ready():
